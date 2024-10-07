aab675efa119e211f8bf04e08cd234a6ed1c75a1
WadeLittle <wdlittle9@gmail.com>
2024-10-07 00:40:40 -0400

    working assignment 11

--- csce242/assignments/09-assignment/script.js
+++ csce242/assignments/09-assignment/script.js
@@ -14,14 +14,35 @@ class Bird {
         overallDiv.classList.add("bird");
         
         const header = document.createElement("h3");
-        header.innerHTML = this.type;  // Use `this.type`
+        header.innerHTML = this.type;
         overallDiv.append(header);
         
         const image = document.createElement("img");
         image.src = "images/" + this.pic;
         overallDiv.append(image);
         
-        document.getElementById("bird-list").append(overallDiv); // Correct `getElementById`
+        // Create the hidden data section
+        const rightSide = document.createElement("section");
+        rightSide.classList.add("bird-data", "hidden"); // Initially hidden
+        rightSide.append(this.paragraph("Wingspan: ", this.wingspan));
+        rightSide.append(this.paragraph("Lifespan", this.lifespan));
+        rightSide.append(this.paragraph("Food", this.food));
+        rightSide.append(this.paragraph("Habitat", this.habitat));
+        rightSide.append(this.paragraph("Interesting Fact: ", this.fact));
+        overallDiv.append(rightSide);
+
+        // Add click event to toggle the visibility of the data
+        overallDiv.onclick = () => {
+            rightSide.classList.toggle("hidden"); // Toggle the hidden class
+        };
+
+        document.getElementById("bird-list").append(overallDiv);
+    }
+
+    paragraph(title, info) {
+        const p = document.createElement("p");
+        p.innerHTML = `<strong>${title}</strong>: ${info}`;
+        return p;
     }
 }
 
@@ -32,6 +53,5 @@ birds.push(new Bird("Peacock", "90 inches", "10-25 years in the wild", "Omnivoro
 birds.push(new Bird("Penguin", "45 inches tall", "Around 15-20 years in the wild", "Primarily fish, but also squid and krill", "Found in Antarctica, living on sea ice and surrounding freezing waters", "Emperor penguins are the only penguin species that breed during the Antarctic winter, with males incubating the eggs for about two months while fasting in extreme conditions", "penguin.jpg"));
 
 birds.forEach((bird) => {
-    console.log(bird.type);  // Use `bird.type` to log the type of each instance
-    bird.item;  // Just call the getter to append the bird item to the DOM
+    bird.item;  // Call the getter to append the bird item to the DOM
 });
